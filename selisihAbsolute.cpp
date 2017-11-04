7b6827beea10408eb9afa163360ac4856aac7845
NadilaMegaSyafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2017-11-04 14:09:55 +0700

    selisih nilai X dan Y

--- selisihAbsolute.cpp
+++ selisihAbsolute.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
+	int x,y,selisih;
+	cout<<" Input Bilangan x : "; cin>>x;
+	cout<<" Input Bilangan y : "; cin>>y;
 
+if (x>y){
+	selisih=x-y;
+	
+	cout<<"Selisih Nilai X dan Y yaitu "<<selisih;
+}
+
+	return 0;
 }
